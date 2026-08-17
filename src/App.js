a747cd2ea9d5ac93cc6a30664a952ba638ee04c6
hajraaziz <hajraaziz10@gmail.com>
2026-08-17 21:27:23 +0500

    handle submiit

--- src/App.js
+++ src/App.js
@@ -20,10 +20,25 @@ function Logo() {
 }
 
 function Form() {
+
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+
   return (
-  <div className="add-form">
+  <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do you need for your 😍 trip?</h3>
-  </div>
+    <select>
+      {Array.from( {length: 20}, (_, i)=> i+1 )
+      .map((num)=> (
+        <option value={num} key={num}>
+          {num}
+        </option>
+      ))}
+    </select>
+    <input type="text" placeholder="Item..."></input>
+    <button>add</button>
+  </form>
   );
 }
 
@@ -32,7 +47,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map( (item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id}/>
         ))}
       </ul>
     </div>
