e5788196ba0cb99106b992964cd5ee9c73aff6f0
hajraaziz <hajraaziz10@gmail.com>
2026-08-31 13:43:44 +0500

    add checkbox

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
-
 export default function App() {
-
   const [items, setItems] = useState([]);
-  
+
   function handleAddItems(item) {
-    setItems((items)=> [...items, item]);
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(idToDelete) {
+    setItems((currentListofItems) =>
+      currentListofItems.filter((item) => item.id !== idToDelete),
+    );
+  }
+
+  function handleToggleItem(idToggled) {
+    setItems((currentListofItems) =>
+      currentListofItems.map((item) =>
+        item.id === idToggled ? { ...item, packed: !item.packed } : item,
+      ),
+    );
   }
 
   return (
     <div className="app">
       <Logo />
-      <Form onAddItems={handleAddItems}/>
-      <PackingList items={items}/>
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
       <Stats />
     </div>
   );
 }
 
 function Logo() {
-  return <h1>🏝️ Far Away🧳</h1>
+  return <h1>🏝️ Far Away🧳</h1>;
 }
 
 function Form({ onAddItems }) {
@@ -37,7 +48,7 @@ function Form({ onAddItems }) {
   //   setItems((items)=> [...items, item]);
   // }
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
 
     if (!description) return;
@@ -52,57 +63,65 @@ function Form({ onAddItems }) {
   }
 
   return (
-  <form className="add-form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
 
-    <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(Number(e.target.value));
+        }}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
 
-    <select 
-    value={quantity} 
-    onChange={(e) => {
-      setQuantity(Number(e.target.value));
-      }}>
-      {Array.from( {length: 20}, (_, i)=> i+1 )
-      .map((num)=> (
-        <option value={num} key={num}>
-          {num}
-        </option>
-      ))}
-    </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
 
-    <input 
-      type="text" 
-      placeholder="Item..." 
-      value={description} 
-      onChange={(e) => {
-        setDescription(e.target.value);
-      }}
-    />
-
-    <button>add</button>
-    
-  </form>
+      <button>add</button>
+    </form>
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
-        {items.map( (item) => (
-          <Item item={item} key={item.id}/>
+        {items.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({item}) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
-      <span style={item.packed ? { textDecoration:"line-through" } : {}}>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
